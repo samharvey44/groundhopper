e26f7d88bdc57c35aac16dade1dea8d0f2f5f9db
Sam <samuelharvey15@hotmail.com>
2022-11-03 18:49:39 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2022: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        'react/prop-types': 'off',
+    },
+};
