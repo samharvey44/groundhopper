a6522065bfd1fcccb93f3209a44fed51932cc92e
Sam <samuelharvey15@hotmail.com>
2022-11-25 21:31:52 +0000

    useCurrent() replace db::raw

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->default(DB::raw('CURRENT_TIMESTAMP'))->nullable();
+            $table->timestamp('email_verified_at')->useCurrent()->nullable();
             $table->string('password');
 
             $table->foreignIdFor(Role::class);
