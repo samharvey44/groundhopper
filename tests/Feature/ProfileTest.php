a48525d9f7dcb36ff20ba5e6290cacad15189ccb
Sam <samuelharvey15@hotmail.com>
2023-01-28 10:13:51 +0000

    test preparation

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -10,18 +10,18 @@ use Tests\TestCase;
 
 use Auth;
 
-class HomeTest extends TestCase
+class ProfileTest extends TestCase
 {
     use RefreshDatabase;
     use SeedsRoles;
     use WithFaker;
 
     /**
-     * Test that an authorized user can view their homescreen, and that it has the right data.
+     * Test that an authorized user can view their profile, and that it has the right data.
      * 
      * @return void
      */
-    public function test_auth_user_can_view_homescreen(): void
+    public function test_auth_user_can_view_profile(): void
     {
         // TODO
     }
